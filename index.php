ba1696b090493787ed5cbdce7a0b60445e07514d
breninho123 <breno.6586483@aluno.mg.gov.br>
2024-12-05 08:41:54 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<h2> receita 
+    <?php
+    echo " brigadeiro";
+    ?>
+    </h2>
+    <H3>  </h3>
+    <table border="1">
+         <TR>
+         <Th> ingredientes</th>
+         <Th> quantidade</th>
+                
+</tr>
+</thead>
+<tbody>
+    <tr>
+</tread>
+<tbody>
+    <tr>
+            <td>leite condençado </td>
+            <td>1 lata</td>
+    </tr>
+    
+    <tr>
+            <td>monteiga </td>
+            <td>2 colheres</td>
+    </tr>
+    
+    <tr>
+            <td> nescau </td>
+            <td>4 colheres</td>
+    </tr>
+</tbody>
+</table> 
+
+<h1> nota: </h1>
+<script>
+    var h1 = document.quergSelector("h1");
+    h1.innerhtml ="nota: 100"
+</script>
